36b017a600ab4c4f62699e3624193cc41034001f
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2026-06-08 17:52:55 +0200

    Move assets inside src folder

--- src/pixel3dmm/env_paths.py
+++ src/pixel3dmm/env_paths.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from environs import Env
 
 REPO_ROOT = f"{Path(__file__).parent.resolve()}/../.."
+PACKAGE_ROOT = f"{Path(__file__).parent.resolve()}"
 
 env = Env(expand_vars=True)
 env_file_path = Path(f"{Path.home()}/.config/pixel3dmm/.env")
@@ -17,18 +18,18 @@ with env.prefixed("PIXEL3DMM_"):
 
 
 
-head_template = f'{CODE_BASE}/assets/head_template.obj'
-head_template_color = f'{CODE_BASE}/assets/head_template_color.obj'
-head_template_ply = f'{CODE_BASE}/assets/test_rigid.ply'
-VALID_VERTICES_WIDE_REGION = f'{CODE_BASE}/assets/uv_valid_verty_noEyes_debug.npy'
-VALID_VERTS_UV_MESH = f'{CODE_BASE}/assets/uv_valid_verty.npy'
-VERTEX_WEIGHT_MASK = f'{CODE_BASE}/assets/flame_vertex_weights.npy'
-MIRROR_INDEX = f'{CODE_BASE}/assets/flame_mirror_index.npy'
-EYE_MASK = f'{CODE_BASE}/assets/uv_mask_eyes.png'
-FLAME_UV_COORDS = f'{CODE_BASE}/assets/flame_uv_coords.npy'
-VALID_VERTS_NARROW = f'{CODE_BASE}/assets/uv_valid_verty_noEyes.npy'
-VALID_VERTS = f'{CODE_BASE}/assets/uv_valid_verty_noEyes_noEyeRegion_debug_wEars.npy'
-FLAME_ASSETS = f'{CODE_BASE}/src/pixel3dmm/preprocessing/MICA/data/'
+head_template = f'{PACKAGE_ROOT}/assets/head_template.obj'
+head_template_color = f'{PACKAGE_ROOT}/assets/head_template_color.obj'
+head_template_ply = f'{PACKAGE_ROOT}/assets/test_rigid.ply'
+VALID_VERTICES_WIDE_REGION = f'{PACKAGE_ROOT}/assets/uv_valid_verty_noEyes_debug.npy'
+VALID_VERTS_UV_MESH = f'{PACKAGE_ROOT}/assets/uv_valid_verty.npy'
+VERTEX_WEIGHT_MASK = f'{PACKAGE_ROOT}/assets/flame_vertex_weights.npy'
+MIRROR_INDEX = f'{PACKAGE_ROOT}/assets/flame_mirror_index.npy'
+EYE_MASK = f'{PACKAGE_ROOT}/assets/uv_mask_eyes.png'
+FLAME_UV_COORDS = f'{PACKAGE_ROOT}/assets/flame_uv_coords.npy'
+VALID_VERTS_NARROW = f'{PACKAGE_ROOT}/assets/uv_valid_verty_noEyes.npy'
+VALID_VERTS = f'{PACKAGE_ROOT}/assets/uv_valid_verty_noEyes_noEyeRegion_debug_wEars.npy'
+FLAME_ASSETS = f'{PACKAGE_ROOT}/src/pixel3dmm/preprocessing/MICA/data/'
 
 # paths to pretrained pixel3dmm checkpoints
 CKPT_UV_PRED = f"{Path.home()}/.cache/pixel3dmm/uv.ckpt"
