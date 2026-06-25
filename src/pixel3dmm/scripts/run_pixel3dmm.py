1d4e55dc3b9189627be0143a113696ec1769ef68
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2026-06-25 11:42:06 +0200

    Cleanup entire p3dmm folder

--- src/pixel3dmm/scripts/run_pixel3dmm.py
+++ src/pixel3dmm/scripts/run_pixel3dmm.py
@@ -93,6 +93,8 @@ def main(video_path: str, preprocessing_folder: str, tracking_folder: str, /, cl
             rmtree(f"{processing_folder}/p3dmm/uv_map")
         if Path(f"{processing_folder}/p3dmm/normals").is_dir():
             rmtree(f"{processing_folder}/p3dmm/normals")
+        if Path(f"{processing_folder}/p3dmm").is_dir():
+            rmtree(f"{processing_folder}/p3dmm")
         if Path(f"{processing_folder}/pipnet").is_dir():
             rmtree(f"{processing_folder}/pipnet")
         if Path(f"{processing_folder}/PIPnet_annotated_images").is_dir():
